614cb939e76333f335e0b5a263f615442b13bc52
Efe Yanar <efe1yanar@gmail.com>
2024-03-05 19:50:15 +0300

    Add files via upload

---
+++ Sum of Numbers/SayılarıTopla/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            Console.Write("Başlangıç sayısını giriniz: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitiş sayısını giriniz: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                int yedek = b;
+                b = a;
+                a = yedek;
+            } 
+            for (int i= a; i <= b; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine("Toplam sayınız: " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
